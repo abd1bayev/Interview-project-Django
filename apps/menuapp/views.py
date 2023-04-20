848f40133f6f61e5fdb9d7e39333a5b052e96de6
abd1bayev <jasurabdibayev0@gmail.com>
2023-04-20 14:20:23 +0500

    update views

--- apps/menuapp/views.py
+++ apps/menuapp/views.py
@@ -1,15 +1,13 @@
-from django.shortcuts import render
-from apps.menuapp.models import Menu
+from django.shortcuts import render, get_object_or_404
+from .models import Menu
 
 
 def home(request):
     menus = Menu.objects.values_list('name', flat=True)
     return render(request, 'home.html', {'menus': menus})
 
+
 def menu(request, menu_name):
-    try:
-        menu = Menu.objects.get(name=menu_name)
-    except Menu.DoesNotExist:
-        menu = None
+    menu = get_object_or_404(Menu, name=menu_name)
 
     return render(request, 'menu/menu.html', {'menu': menu})
